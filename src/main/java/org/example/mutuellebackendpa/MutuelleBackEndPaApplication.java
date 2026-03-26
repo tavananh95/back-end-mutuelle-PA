8dee1486ca4c185bf7edc14f39a00d86ad3c4bed
vananh_ta <tavananh95@gmail.com>
2026-03-26 10:23:55 +0100

    Init archi

---
+++ src/main/java/org/example/mutuellebackendpa/MutuelleBackEndPaApplication.java
@@ -0,0 +1,13 @@
+package org.example.mutuellebackendpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutuelleBackEndPaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutuelleBackEndPaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/mutuellebackendpa/MutuelleBackEndPaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mutuellebackendpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutuelleBackEndPaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
